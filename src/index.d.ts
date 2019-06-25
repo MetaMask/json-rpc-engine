c863d7a440aaa16586ba4fc423cd0f69da3b5b1c
Dan Finlay <dan@danfinlay.com>
2019-06-25 09:40:19 -0700

    Fixed type defs

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,37 +1,38 @@
-/// <reference types="RpcEngine" />
-
 /** A String specifying the version of the JSON-RPC protocol. MUST be exactly "2.0". */
 export type JsonRpcVersion = "2.0";
 
-/** Method names that begin with the word rpc followed by a period character 
+/** Method names that begin with the word rpc followed by a period character
  * (U+002E or ASCII 46) are reserved for rpc-internal methods and extensions
  *  and MUST NOT be used for anything else. */
 export type JsonRpcReservedMethod = string;
 
 /** An identifier established by the Client that MUST contain a String, Number,
- *  or NULL value if included. If it is not included it is assumed to be a 
+ *  or NULL value if included. If it is not included it is assumed to be a
  *  notification. The value SHOULD normally not be Null and Numbers SHOULD
  *  NOT contain fractional parts [2] */
 export type JsonRpcId = number | string | void;
 
 interface JsonRpcRequest<T> {
   jsonrpc: JsonRpcVersion;
+  method: string;
   id: JsonRpcId;
-  params?: T; 
+  params?: T;
 }
 
 interface JsonRpcNotification<T> extends JsonRpcResponse<T> {
   jsonrpc: JsonRpcVersion;
-  params?: T; 
+  params?: T;
 }
 
 interface JsonRpcResponse<T> {
+  result?: any;
+  error?: JsonRpcError<any>;
   jsonrpc: JsonRpcVersion;
   id: JsonRpcId;
 }
 
 interface JsonRpcSuccess<T> extends JsonRpcResponse<T> {
-    result: T;
+    result: any;
 }
 
 interface JsonRpcFailure<T> extends JsonRpcResponse<T> {
@@ -46,23 +47,19 @@ interface JsonRpcError<T> {
 }
 
 type JsonRpcEngineEndCallback = (error?: JsonRpcError<any>) => void;
+type JsonRpcEngineNextCallback = (returnFlightCallback?: (done: () => void) => void) => void;
 
 interface JsonRpcMiddleware {
   (
-    req: JsonRpcRequest<any[]>,
-    res: JsonRpcResponse<any[]>,
-    next: (returnFlightCallback?: (res: JsonRpcResponse<any>) => void) => void,
+    req: JsonRpcRequest<any>,
+    res: JsonRpcResponse<any>,
+    next: JsonRpcEngineNextCallback,
     end: JsonRpcEngineEndCallback,
   ) : void;
 }
 
-interface IRpcEngine {
+interface JsonRpcEngine {
   push: (middleware: JsonRpcMiddleware) => void;
-  handle: (req: JsonRpcRequest, cb: (err: JsonRpcError?, res: JsonRpcResponse) => void) => void;
-}
-
-export declare class RpcEngine implements IRpcEngine {};
-  push: (middleware: JsonRpcMiddleware) => void;
-  handle: (req: JsonRpcRequest, cb: (err: JsonRpcError?, res: JsonRpcResponse) => void) => void;
+  handle: (req: JsonRpcRequest<any>, callback: (error: JsonRpcError<any>, res: JsonRpcResponse<any>) => void) => void;
 }
 
