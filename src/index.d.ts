73a3a04c77b67896760b71e7ae63bc2686ea13b7
Erik Marks <rekmarks@protonmail.com>
2020-10-25 17:04:17 -0700

    fixup! Remove needlessly complicated XOR type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,24 @@
 
 import { IEthereumRpcError } from 'eth-rpc-errors/@types'
 
-/** A String specifying the version of the JSON-RPC protocol. MUST be exactly "2.0". */
+/**
+ * A String specifying the version of the JSON-RPC protocol.
+ * MUST be exactly "2.0".
+ */
 export type JsonRpcVersion = "2.0";
 
 /** Method names that begin with the word rpc followed by a period character
  * (U+002E or ASCII 46) are reserved for rpc-internal methods and extensions
- *  and MUST NOT be used for anything else. */
+ * and MUST NOT be used for anything else.
+ */
 export type JsonRpcReservedMethod = string;
 
-/** An identifier established by the Client that MUST contain a String, Number,
- *  or NULL value if included. If it is not included it is assumed to be a
- *  notification. The value SHOULD normally not be Null and Numbers SHOULD
- *  NOT contain fractional parts [2] */
+/**
+ * An identifier established by the Client that MUST contain a String, Number,
+ * or NULL value if included. If it is not included it is assumed to be a
+ * notification. The value SHOULD normally not be Null and Numbers SHOULD
+ * NOT contain fractional parts.
+ */
 export type JsonRpcId = number | string | void;
 
 export interface JsonRpcError<T> extends IEthereumRpcError<T> {}
