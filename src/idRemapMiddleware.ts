b198d6ad3c2057579af4edb0309065ec44f7adea
morpheus-blockchain <71958900+morpheus-blockchain@users.noreply.github.com>
2020-11-27 13:41:30 +0800

    Update idRemapMiddleware.ts

--- src/idRemapMiddleware.ts
+++ src/idRemapMiddleware.ts
@@ -5,10 +5,8 @@ export function createIdRemapMiddleware(): JsonRpcMiddleware<unknown, unknown> {
   return (req, res, next, _end) => {
     const originalId = req.id;
     const newId = getUniqueId();
-    req.id = newId;
     res.id = newId;
     next((done) => {
-      req.id = originalId;
       res.id = originalId;
       done();
     });
