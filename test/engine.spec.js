e545ae0b1bb2bc49a74a98b68621d49874899f58
niranjanabinoy <niranjanabinoy@gmail.com>
2020-12-16 18:34:09 -0500

    removing the import/order fix in the test file

--- test/engine.spec.js
+++ test/engine.spec.js
@@ -2,8 +2,8 @@
 'use strict';
 
 const { strict: assert } = require('assert');
-const { JsonRpcEngine } = require('../dist');
 const { stub } = require('sinon');
+const { JsonRpcEngine } = require('../dist');
 
 describe('JsonRpcEngine', function () {
   it('handle: throws on truthy, non-function callback', function () {
