351cc52d1abc088a3cdbcd4d057f6df61aa2fea9
Erik Marks <rekmarks@protonmail.com>
2022-04-07 16:33:47 -0700

    Update dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   root: true,
 
-  plugins: ['import'],
-
-  extends: ['@metamask/eslint-config', '@metamask/eslint-config-nodejs'],
+  extends: ['@metamask/eslint-config'],
 
   rules: {
     'prefer-object-spread': 'off',
@@ -14,6 +12,15 @@ module.exports = {
       files: ['*.ts'],
       extends: ['@metamask/eslint-config-typescript'],
     },
+
+    {
+      files: ['*.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      extends: ['@metamask/eslint-config-nodejs'],
+    },
+
     {
       files: ['test/*'],
       extends: ['@metamask/eslint-config-mocha'],
